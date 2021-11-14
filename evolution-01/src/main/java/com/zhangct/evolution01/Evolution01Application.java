f3b496f05933108a40373543881b156ac6a1adb4
root <31872379@qq.com>
2021-11-14 11:02:58 +0800

    初始化

---
+++ evolution-01/src/main/java/com/zhangct/evolution01/Evolution01Application.java
@@ -0,0 +1,13 @@
+package com.zhangct.evolution01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evolution01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Evolution01Application.class, args);
+    }
+
+}
---
+++ monolithic/src/test/java/com/zhangct/monolithic/MonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangct.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
